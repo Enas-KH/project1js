dbaf6eada2d4c33cc92950ded7de76ff91555589
Enas-KH <enkhowimer@gmail.com>
2022-07-24 16:19:56 +0300

    dayunknown

--- js/script.js
+++ js/script.js
@@ -47,16 +47,17 @@ let Books = [
 
 let fav = [];
 const ss = document.getElementsByClassName("favs")[0];
-const display= (obj , arrayname) => {
+const display = (obj, arrayname) => {
     let temp = document.createElement('div');
     let titleCell = document.createElement('h4');
-    let authorCell = document.createElement('h6');
+    let authorCell = document.createElement('h5');
     let imageCell = document.createElement('img');
     let button = document.createElement('button');
+    button.setAttribute("class","button");
     const hiddenId = document.createElement('input');
     imageCell.src = obj.image;
     titleCell.innerText = obj.title;
-    authorCell.innerText = obj.author; 
+    authorCell.innerText = obj.author;
     imageCell.setAttribute("class", "image"); // imageCell.classList.add('image')
     titleCell.setAttribute("class", "title");
     authorCell.setAttribute("class", "author");
@@ -65,73 +66,77 @@ const display= (obj , arrayname) => {
     temp.appendChild(imageCell);
     temp.appendChild(titleCell);
     temp.appendChild(authorCell);
-    temp.appendChild(button); 
-    
+    temp.appendChild(button);
+
+
 
     if (arrayname === "Books") {
         document.getElementsByClassName("results")[0].appendChild(temp);
-        button.innerText = "Add to favorites";
+        button.setAttribute("class","button fas fa-book-heart")
         button.addEventListener("click", e => {
             addtofavourites(obj)
         })
     }
     else {
         ss.appendChild(temp);
-        button.innerText="delete from favourites";
+        button.setAttribute("class","button fas fa-trash")
         button.addEventListener("click", e => {
             removefromfvourites(obj)
         })
     }
 };
 
-//renderBooks("Books");
 
-let addtofavourites= obj => { //arrow function
+let addtofavourites = obj => { //arrow function
     fav = JSON.parse(localStorage.getItem("fav")) || [];
     fav.push(obj);
     localStorage.setItem("fav", JSON.stringify(fav));
 }
 
-let removefromfvourites=obj=> {                      
+let removefromfvourites = obj => {
     console.log(obj.id) //console.log(el.id)
     fav = JSON.parse(localStorage.getItem('fav')) || [];
-    fav= fav.filter((el)=> {el.id !== obj.id  })
+    fav = fav.filter((el) => { el.id !== obj.id })
     localStorage.setItem("fav", JSON.stringify(fav));
     ss.innerHTML = "";
     render(fav, "fav");
 
 }
 
-let render=(array, arrayname)=> {
-    array.map((obj) => { display(obj,arrayname) });
+let render = (array, arrayname) => {
+    array.map((obj) => { display(obj, arrayname) });
 }
-render(Books, "Books"); 
+render(Books, "Books");
 
 function openForm() {
     document.getElementById("myForm").style.display = "block";
-  }
-  
-  function closeForm() {
+}
+
+function closeForm() {
     document.getElementById("myForm").style.display = "none";
-  }
+}
+console.log(document.getElementById('newBook'));
 document.getElementById('newBook').addEventListener('click', e => {
-   e.preventDefault();
+    console.log('click');
+    e.preventDefault();
     AddNewBook(e);
 })
 
-function AddNewBook({target: {parentNode}}) {
+
+console.log(document.getElementById("Books"));
+function AddNewBook({ target: { parentNode } }) {
     const title = parentNode[0].value;
     const author = parentNode[1].value;
     const edition = parentNode[2].value;
-    const image = parentNode[3].value;
+    let image = parentNode[3].value;
     const id = Books.length;
-    const path = "img/"
-    image=path.concat(JSON.stringify(image).slice(10,-10))
+    //const path = "img/"
+    //image = path.concat(JSON.stringify(image).slice(10, -10))
     const newBook = { id, title, author, edition, image };
     closeForm();
     Books.push(newBook);
-    document.getElementById("Books").innerHTML = "";
-    renderBooks("Books");
+    document.getElementsByClassName("results")[0].innerHTML = "";
+    render(Books,"Books");
 }
 
 
